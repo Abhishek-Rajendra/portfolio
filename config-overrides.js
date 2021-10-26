fca1b3abbfcbe270d2acbc3b0aa7c90a864516d5
Rajendra Prasad <rabhi1604@gmail.com>
2021-10-26 13:19:25 -0500

    Proxy server

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
-const {alias, configPaths} = require('react-app-rewire-alias')
+const { alias, configPaths } = require("react-app-rewire-alias");
 
-const aliasMap = configPaths('./jsconfig.paths.json') // or jsconfig.paths.json
+const aliasMap = configPaths("./jsconfig.paths.json"); // or jsconfig.paths.json
 
-module.exports = alias(aliasMap)
+module.exports = alias(aliasMap);
 
 const { exec } = require("child_process");
 exec(`node ${__dirname}/server.js`);
