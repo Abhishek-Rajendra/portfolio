9c46f69ff91f2ab7cbc2687fee653541037619ed
abhishek-rajendra <rabhi1604@gmail.com>
2021-11-05 03:07:27 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withImages = require("next-images");
 
 module.exports = withImages(
   withVideos({
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     reactStrictMode: true,
     images: {
       disableStaticImages: true,
