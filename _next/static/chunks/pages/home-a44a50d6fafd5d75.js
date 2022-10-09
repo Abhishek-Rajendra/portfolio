fe91eaf37284729ea19707181c2a9c4898c91d57
Abhishek-Rajendra <36518069+Abhishek-Rajendra@users.noreply.github.com>
2022-10-09 04:38:26 +0000

    Deploying to gh-pages from @ Abhishek-Rajendra/portfolio@1d4228e7c000552ead91ee8cece0e781da894118 🚀

---
+++ _next/static/chunks/pages/home-a44a50d6fafd5d75.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{8946:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(7141)}])}},function(n){n.O(0,[885,427,961,141,774,888,179],(function(){return _=8946,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
