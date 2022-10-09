a8268457f4d6983cc915433f675b16560e38f849
Abhishek-Rajendra <rabhi1604@gmail.com>
2022-10-08 23:17:40 -0500

    updated resume

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,6 +4,9 @@ import { Image, Paragraph, Title } from "@components/Components";
 import styled from "styled-components";
 import aboutMe from "@constants/home";
 
+import Abhishek_Rajendra_MSCS from "@assets/Abhishek_Rajendra_MSCS.pdf";
+import Abhishek_Rajendra_MSCS_ML from "@assets/Abhishek_Rajendra_MSCS_ML.pdf";
+
 const HomeContainer = styled.div`
   padding: 50px;
 `;
@@ -23,6 +26,9 @@ const Home = () => {
           <Row>
             <Col xs={12} md={8}>
               <Paragraph data={aboutMe}></Paragraph>
+              <a href={Abhishek_Rajendra_MSCS} target="_blank" rel="noreferrer">Resume for Software Engineer</a>
+              <br></br>
+              <a href={Abhishek_Rajendra_MSCS_ML} target="_blank" rel="noreferrer">Resume for Data Science/ML/Research</a>
             </Col>
             <Col xs={6} md={4}>
               <Image src={abhishek} alt="Profile Photo" {...options}></Image>
